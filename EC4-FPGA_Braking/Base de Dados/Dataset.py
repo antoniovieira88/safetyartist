29bf021a558a7b11e99b27a74ed1c55025d2ca74
antoniovieira88 <71773163+antoniovieira88@users.noreply.github.com>
2022-06-09 12:24:21 -0300

    EC4: [p/Henrique] Base de dados com sensores ruidosos e melhoria de desempenho.

--- EC4-FPGA_Braking/Base de Dados/Dataset.py
+++ EC4-FPGA_Braking/Base de Dados/Dataset.py
@@ -1,5 +1,9 @@
 import pandas as pd
 import numpy as np
+np.random.seed(0)
+
+import time
+start = time.time()
 
 distance = np.arange(start=0, stop=2010, step=10)   #Cria vetor distancia [m]
 speed = np.arange(start=0, stop=28.5, step=0.5)     #Cria vetor velocidade [m/s]
@@ -43,22 +47,37 @@ T50 = 0.5       #Tempo de duracao da aplicacao do freio com metade da capacidade
 
 def create_Dataset(nome, distancia, velocidade, desaceleracao, massa):
 
-    data = pd.DataFrame(columns=['Distancia', 'Velocidade', 'Capacidade de Frenagem', 'Decisao', 'AW', 'AG', 'VH', 'VC', 'V50', 'DS']) #Cria um Dataframe vazio com as colunas descritas
+    size = len(distancia)*len(velocidade)*len(desaceleracao)
+
+    data = pd.DataFrame(columns=['Distancia Ruidosa', 'Velocidade Ruidosa', 'Capacidade de Frenagem Ruidosa', 'Distancia', 'Velocidade', 'Capacidade de Frenagem', 'Decisao', 'Aceleracao', 'AW', 'AG', 'VH', 'VC', 'V50', 'DS'], index = range(size)) #Cria um Dataframe vazio com as colunas descritas
 
     M = massa 
-    l = 0 #Serve apenas para contar em qual linhas do Dataframe 
+    l = 0 #Contador de linhas
 
     for i in range(len(distancia)):
         for j in range(len(velocidade)):
             for k in range(len(desaceleracao)):
-                l += 1 
-                if(brake[k] > 0.8): #Se a desaceleração for de aderencia normal
-                    AH = 1.12 #Utilizar aderencia normal
-                else:
-                    AH = 0.84 #Utilizar baixa aderencia
-                data.loc[l] = np.concatenate((np.array([distance[i], speed[j], brake[k]]), calcula_distancia(distance[i],speed[j],brake[k])), axis = None) #Escreve o como uma linha no dataframe
+                #Calcula o desvio padrão da distância
+                if(distance[i] <= 30):
+                    devpadDistancia = 0.5
+                else: 
+                    devpadDistancia = 5
+                distanciaRuido = np.random.normal(distance[i], devpadDistancia) #Gera o ruído
+                if(distanciaRuido < 0):
+                    distanciaRuido = 0
+                
+                devpadVelocidade = 0.03*speed[j] #Calcula o desvio padrão da velocidade
+                velocidadeRuido = np.random.normal(speed[j], devpadVelocidade) #Gera o ruído
+                if(velocidadeRuido < 0):
+                    velocidadeRuido = 0
 
-    dataCompressed = data[['Distancia', 'Velocidade', 'Capacidade de Frenagem', 'Decisao']] #Cria um segundo dataframe reduzindo o número de colunas
+                ruidos = np.array([distanciaRuido, velocidadeRuido, brake[k]]) #Define vetores para facilitar a chamada do concatenate
+                sinais = np.array([distance[i], speed[j], brake[k]])
+
+                data.loc[l] = np.concatenate((ruidos, sinais, calcula_distancia(distance[i],speed[j],brake[k])), axis = None) #Escreve o como uma linha no dataframe
+                l += 1 
+
+    dataCompressed = data[['Distancia Ruidosa', 'Velocidade Ruidosa', 'Capacidade de Frenagem Ruidosa', 'Decisao']] #Cria um segundo dataframe reduzindo o número de colunas
 
     data.to_csv(nome + "Debug.csv", header = False, index = False) #Cria o dataframe completo em csv
     dataCompressed.to_csv(nome + ".csv", header = False, index = False) #Cria o dataframe reduzido em csv
@@ -73,6 +92,11 @@ def calcula_distancia(distancia, velocidade, aceleracao):
     VE = np.double(velocidade)
     A = np.double(aceleracao)
 
+    if(aceleracao > 0.8): #Se a desaceleração for de aderencia normal
+        AH = 1.12 #Utilizar aderencia normal
+    else:
+        AH = 0.84 #Utilizar baixa aderencia
+
     AW = (0.5*RHO*Cd*ATS*(VW-VE)**2)/(M+MRI)
     AG = (M*np.sin(np.arctan(GR))*g)/(M+MRI)
     VH = VE + (AH+AW-AG)*TH
@@ -81,10 +105,11 @@ def calcula_distancia(distancia, velocidade, aceleracao):
     DS = ((VE*TH)+(0.5*(AH+AW-AG)*TH**2))+((VH*TC)+(0.5*(AW-AG)*TC**2))+((VC*T50)+(0.5*(0.5*A+AW-AG)*T50**2))+(V50**2/(2*(A+AW+AG)))
 
     if DS >= D: #Se a distancia para parada >= distancia para obstaculo
-        return 1, AW, AG, VH, VC, V50, DS #Frenagem necessaria
+        return 1, AH, AW, AG, VH, VC, V50, DS #Frenagem necessaria
     else:   
-        return 0, AW, AG, VH, VC, V50, DS #Frenagem desnecessaria
+        return 0, AH, AW, AG, VH, VC, V50, DS #Frenagem desnecessaria
 
 create_Dataset("Dataset", distance, speed, brake, 246898)   ## Cria dataset para trem vazio, para calcular outros basta Copy/Paste e trocar o nome e valor da massa
 
-
+end = time.time()
+print("Tempo de execução: ", end - start)
