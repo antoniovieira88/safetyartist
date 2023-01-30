35701d1a4efa697084eaea004f15b5887a6fdd5a
Antonio Vieira da Silva Neto <antonio.vieira88@usp.br>
2023-01-30 20:17:32 -0300

    EC3: Added new test to assess the pseudorandom generator. A test to check its uniform distribution within [0; 1] has been coded, added and successfully performed. The results are on the file '/tests/RandomGeneratorMt19937/UnityDistributionResults.txt'.

--- EC3-C++_Fault_Detection/tests/RandomGeneratorMt19937/include/RandomGeneratorTest.h
+++ EC3-C++_Fault_Detection/tests/RandomGeneratorMt19937/include/RandomGeneratorTest.h
@@ -5,4 +5,5 @@
 #include "../../../src/utils/mt_engine/include/MtRandEngine.h"
 
 using namespace std;
-int RandomGeneratorTest();
+int RandomGeneratorStateTest();
+void RandomGeneratorUnityUniformityTest();
